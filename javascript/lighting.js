b64c7a77e332cf8bf7be8019b47e13e41671b20a
shrubfromtomorrow <orionjandrews@gmail.com>
2022-11-19 21:21:52 -0800

    Committing to publish site

--- javascript/lighting.js
+++ javascript/lighting.js
@@ -59,4 +59,31 @@ seniorLinkList.addEventListener("click", () => {
   }
 });
 
+const buttons = document.querySelectorAll("[data-carousel-button]");
+const caption = document.querySelector("#caption");
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offest = button.dataset.carouselButton === "next" ? 1 : -1;
+    const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activeSlide) + offest;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+    if (newIndex == 0) {
+      caption.textContent = "The Setup"
+    }
+    else if (newIndex == 1) {
+      caption.textContent = "Rotation"
+    }
+    else if (newIndex == 2) {
+      caption.textContent = "Pixeling"
+    }
+    else {
+      caption.textContent = "Doubled Lines"
+    }
+  })
+})
+
 
